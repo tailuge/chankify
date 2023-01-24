2085f22b84c74924101a954cbed713b57209b7b5
tailuge <tailuge@gmail.com>
2023-01-24 11:32:33 +0000

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
-
-function MyForm() {
-  const [inputText, setInputText] = useState('');
-  const [displayText, setDisplayText] = useState('');
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    setDisplayText(inputText);
-  };
-
-  return (
-    <div>
-      <input type="text" onChange={handleInputChange} value={inputText} />
-      <button onClick={handleButtonClick}>Echo</button>
-      {displayText !== '' && (
-        <div style={{ backgroundColor: 'lightgray', padding: '1rem' }}>
-          <code>{displayText}</code>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default MyForm;
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+ReactDOM.render(
+<div>
+<h1>Hello, Welcome to React and TypeScript</h1>
+</div>,
+  document.getElementById("root")
+);
