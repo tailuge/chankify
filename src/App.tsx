efeb682fc9ae467357e88cf41e3cef85c38c1981
tailuge <tailuge@gmail.com>
2023-01-27 02:11:33 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. 
+          {`dict: ${JSON.stringify(dict)}`}
         </p>
         <a
           className="App-link"
