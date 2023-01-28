3783f4baeb51a36622ff95cc397196a22368cad2
tailuge <tailuge@gmail.com>
2023-01-28 08:56:04 +0000

    basic input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import './App.css';
-import dict from './dict.json';
-import { Translate } from './Translate';
-import { Entry } from './Entry';
 import { VocabularyForm } from './VocabularyForm';
 
-
 function App() {
-
-  const translate = new Translate(dict as Entry[]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {`dict: ${JSON.stringify(translate.getVocab("近來「AI製圖」在網上蔚為一陣風潮"))}`}
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/tailuge/chankify"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Chankify
         </a>
         <VocabularyForm />
       </header>
--- src/VocabularyForm.tsx
+++ src/VocabularyForm.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import { VocabularyFormState } from './VocabularyFormState';
+import dict from './dict.json';
+import { Translate } from './Translate';
+import { Entry } from './Entry';
 
 export class VocabularyForm extends React.Component<{}, VocabularyFormState>
 {
 
+
+  translate = new Translate(dict as Entry[])
   state: VocabularyFormState = { inputText: '', outputText: '' }
 
-  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     this.setState({ inputText: event.target.value, outputText: '' });
   };
 
 
   handleButtonClick = () => {
-    alert(this.state.inputText);
+    const vocab = this.translate.getVocab(this.state.inputText)
+    const text = vocab.map(entry => `${entry.hanzi}\t${entry.pinyin}\t${entry.meaning}`).join("\n")
+    this.setState({ inputText: this.state.inputText, outputText: text })
   };
 
   render() {
-    return (<div>
-      <label htmlFor="firstName">First Name</label>
-      <input name="firstName" value={this.state.inputText} onChange={this.handleInputChange} />
-      <button onClick={this.handleButtonClick}>Echo</button>
+    return (
 
-    </div>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', padding:'20px', width:'100%' , border:'20px' }}>
+        <div style={{ flex: 1 }}>
+          <textarea rows={5} value={this.state.inputText} style={{ width: '100%' }} onChange={this.handleInputChange}></textarea>
+          <br />
+          <button onClick={this.handleButtonClick}>Extract Vocabulary</button>
+        </div>
+        <div style={{ flex: 1 }}>
+          <pre>{this.state.outputText}</pre>
+        </div>
+      </div>
+
     )
   }
 }
